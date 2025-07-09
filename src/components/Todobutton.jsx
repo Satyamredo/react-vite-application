ef012640c959c640f8e5cf80463f319794c8b653
Satyam <satyamkumarsingh0154@gmail.com>
2025-07-09 20:59:16 +0530

    created components in application

---
+++ src/components/Todobutton.jsx
@@ -0,0 +1,5 @@
+const Button = () => {
+    return <button>Add Todo</button>;
+};
+
+export default Button;
---
+++ src/components/Todoitem.jsx
@@ -0,0 +1,13 @@
+const Todoitem = () => {
+    return (
+        <li className="todo-item">
+            <span>
+                <input type="checkbox" />
+                <span>Eat</span>
+            </span>
+            <p>...</p>
+        </li>
+    );
+};
+
+export default Todoitem;
